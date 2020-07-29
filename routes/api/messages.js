c0960206e7c503f0a5bf07e51de8418d78689330
anshusaurav <anshu.saurav@gmail.com>
2020-07-30 00:54:26 +0530

    Specific User to and from msg

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -29,6 +29,29 @@ router.post("/:username/:slug", auth.required, function (req, res, next) {
 
 })
 router.get('/listUsers', auth.required, function (req, res, next) {
+    // const id = req.payload.id;
+    // Promise.all([
+    //     Message.find({ $or: [{ sender: id }, { receiver: id }] })
+
+    //         .populate("sender")
+    //         .populate("receiver")
+
+    //         .exec()
+    // ])
+
+    //     .then(function (results) {
+    //         var users = results[0];
+    //         console.log(results[0]);
+    //         User.findById(id).then(function (user) {
+    //             return res.json({
+    //                 users: users.map(function (userM) {
+    //                     console.log(userM);
+    //                     return userM.toJSONFor(user)
+    //                 })
+    //             })
+    //         })
+
+    //     })
     const id = req.payload.id;
     Message.find().or([{ sender: id }, { receiver: id }])
         .distinct("sender")
@@ -38,6 +61,42 @@ router.get('/listUsers', auth.required, function (req, res, next) {
             // const set = new Set(ids);
             return res.json({ messages: messages })
         })
+
+})
+
+router.get('/:userId', auth.required, function (req, res, next) {
+    const id = req.payload.id;
+    const { userId } = req.params;
+    User.findById(req.payload.id).then(function (user) {
+        Promise.all([
+            Message.find({
+                $or: [
+                    { $and: [{ sender: id }, { receiver: userId }] },
+                    { $and: [{ sender: userId }, { receriver: id }] }
+                ]
+            }).sort({ createdAt: "desc" }).populate('sender').populate('receiver').populate('post').exec(),
+            Message.count({
+                $or: [
+                    { $and: [{ sender: id }, { receiver: userId }] },
+                    { $and: [{ sender: userId }, { receriver: id }] }
+                ]
+            }).exec()
+        ])
+            .then(function (results) {
+                const messages = results[0];
+                const messageCount = results[1];
+                // console.log(messages);
+                return res.json({
+                    messages: messages.map(function (message) {
+                        return { message };
+                    }),
+                    messageCount
+
+                })
+
+            })
+    })
+
 
 })
 module.exports = router;
